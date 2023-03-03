f4bca1cd82144cae9e12ddb2489ecde03a96c11d
Stephen An <anyang01@qq.com>
2023-03-03 19:27:00 +0800

    修改说明
    修改说明

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -49,9 +49,6 @@ onMounted(() => {
           </a>
           ，免费且基于 MIT 协议，没有任何形式的付费行为！
         </p>
-        <p>
-          如果你觉得此项目对你有帮助，请在 Github 帮我点个 Star 或者给予一点赞助，谢谢！
-        </p>
       </div>
       <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
       <p>{{ $t("setting.reverseProxy") }}：{{ config?.reverseProxy ?? '-' }}</p>
